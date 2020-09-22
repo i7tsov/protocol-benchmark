19fbccf494cd07a50c0d61d977d62939cc154d95
i7tsov <isementsov@infoblox.com>
2020-09-22 21:28:51 +0300

    Updated benchmark

--- grpc_test.go
+++ grpc_test.go
@@ -12,71 +12,90 @@ import (
 	"google.golang.org/grpc/test/bufconn"
 )
 
-func BenchmarkGRPCMem(b *testing.B) {
-	var msgCount = b.N
-	serv := bgrpc.Server{
-		Elements: bgrpc.Generate(msgCount),
-	}
-
-	const bufSize = 64 * 1024
-	lis := bufconn.Listen(bufSize)
-	s := grpc.NewServer()
-	pb.RegisterServerServer(s, serv)
-	go func() {
-		if err := s.Serve(lis); err != nil {
-			log.Fatalf("Error: %v", err)
-		}
-	}()
-
-	conn, err := grpc.DialContext(context.Background(), "bufconn",
-		grpc.WithContextDialer(func(context.Context, string) (net.Conn, error) {
-			return lis.Dial()
-		}),
-		grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Error: %v", err)
-	}
-	defer conn.Close()
-
-	client := bgrpc.Client{
-		Conn: conn,
-	}
-	elements, err := client.Download()
-	if err != nil {
-		log.Fatalf("Error: %v", err)
-	}
-	_ = elements
+func BenchmarkGRPCMem100k(b *testing.B) {
+	runGRPCBench(100000, b.N, false)
 }
 
-func BenchmarkGRPCNet(b *testing.B) {
-	var msgCount = b.N
+func BenchmarkGRPCMem10k(b *testing.B) {
+	runGRPCBench(10000, b.N, false)
+}
+
+func BenchmarkGRPCMem1k(b *testing.B) {
+	runGRPCBench(1000, b.N, false)
+}
+
+func BenchmarkGRPCMem10(b *testing.B) {
+	runGRPCBench(10, b.N, false)
+}
+
+func BenchmarkGRPCNet100k(b *testing.B) {
+	runGRPCBench(100000, b.N, true)
+}
+
+func BenchmarkGRPCNet10k(b *testing.B) {
+	runGRPCBench(10000, b.N, true)
+}
+
+func BenchmarkGRPCNet1k(b *testing.B) {
+	runGRPCBench(1000, b.N, true)
+}
+
+func BenchmarkGRPCNet10(b *testing.B) {
+	runGRPCBench(10, b.N, true)
+}
+
+func runGRPCBench(msgCount, num int, useNet bool) {
 	serv := bgrpc.Server{
 		Elements: bgrpc.Generate(msgCount),
 	}
 
+	var conn *grpc.ClientConn
+	var err error
 	s := grpc.NewServer()
 	pb.RegisterServerServer(s, serv)
-	lis, err := net.Listen("tcp", ":9091")
-	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
+	if useNet {
+		lis, err := net.Listen("tcp", ":9091")
+		if err != nil {
+			log.Fatalf("Failed to listen: %v", err)
+		}
+		go s.Serve(lis)
+		conn, err = grpc.Dial("localhost:9091", grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+		}
+	} else {
+		const bufSize = 64 * 1024
+		lis := bufconn.Listen(bufSize)
+		go func() {
+			if err := s.Serve(lis); err != nil {
+				log.Fatalf("Error: %v", err)
+			}
+		}()
+		conn, err = grpc.DialContext(context.Background(), "bufconn",
+			grpc.WithContextDialer(func(context.Context, string) (net.Conn, error) {
+				return lis.Dial()
+			}),
+			grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+		}
 	}
-	go s.Serve(lis)
 
-	dialOpt := grpc.WithInsecure()
-	conn, err := grpc.Dial("localhost:9091", dialOpt)
-	if err != nil {
-		log.Fatalf("Error: %v", err)
-	}
 	defer conn.Close()
 
 	client := bgrpc.Client{
 		Conn: conn,
 	}
-	elements, err := client.Download()
-	if err != nil {
-		log.Fatalf("Error: %v", err)
-	}
-	_ = elements
 
-	s.Stop()
+	for i := 0; i < num; i++ {
+		elements, err := client.Download()
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+		}
+		_ = elements
+	}
+
+	if useNet {
+		s.Stop()
+	}
 }
